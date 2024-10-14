334e4c6c9358e4f91c9bb49e2227c1e847c3fe26
Gianluca-Milesi <gianluca.milesi.99@gmail.com>
2024-10-14 09:59:34 +0200

    ADD: exercise complete

--- 02-strings/script.js
+++ 02-strings/script.js
@@ -1,11 +1,20 @@
 // 1. Dichiarazione di variabili
 // Crea una variabile chiamata name e assegnale il tuo nome.
 // Stampa il valore di nome nella console.
+const name = "Gianluca"; //string
+console.log(name);
+
 
 // 2. Tipi primitivi
 // Crea tre variabili chiamate number, text e bool e assegna rispettivamente
 // un numero, una stringa e un valore booleano.
 // Stampa tutti i valori nella console.
+const number = 50; //number
+let text = "ciao"; //string
+let bool = true; //boolean
+
+console.log(number, text, bool)
+
 
 // 3. Sostituisci null nelle righe sottostanti utilizzando operazioni con le stringhe
 // e le variabili sopra in modo da stampare correttamente 
@@ -13,11 +22,11 @@
 const alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'; // NON MODIFICARE QUESTA RIGA
 const firstName = 'Jane'; // NON MODIFICARE QUESTA RIGA
 const lastName = 'Smith'; // NON MODIFICARE QUESTA RIGA
-
-const fullName = null;
+ 
+const fullName = "Jane" + " " + "Smith";
 console.log(fullName); // Output: "Jane Smith"
 
 // 4. Imposta questa variabile utilizzando una proprietà
 // sulla variabile alphabet per ottenere la sua lunghezza
-const numberOfLettersInAlphabet = null;
-console.log(numberOfLettersInAlphabet); // Output: 26
+const numberOfLettersInAlphabet = "abcdefghijklmnopqrstuvwxyz"; //string
+console.log(numberOfLettersInAlphabet.length); // Output: 26
